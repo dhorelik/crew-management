32cb48bced9142ce8c8f8ddee230b77f6fc77060
Dmytro Gorelik <freelancer2go@gmail.com>
2018-09-21 08:16:19 +0300

    adjust package versions

--- src/components/Routes/component.js
+++ src/components/Routes/component.js
@@ -7,7 +7,7 @@ import * as routePaths from 'constants/routePaths'
 const Routes = () => (
     <Switch>
         <Route exact path={routePaths.HOME} component={screens.Home} />
-        // TODO add Candidate screen
+        {/* TODO add Candidate screen */}
 
         <Route component={screens.NotFound} />
     </Switch>
